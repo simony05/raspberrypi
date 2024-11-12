a590ed5ba6f7bb57d186fcc7316a7bf515386ee9
simony05 <67559452+simony05@users.noreply.github.com>
2024-11-11 23:16:19 -0800

    edits

--- onnx2tf.py
+++ onnx2tf.py
@@ -1,13 +1,2 @@
-import onnx
-from onnx_tf.backend import prepare
-import tensorflow as tf
-
-onnx_model = onnx.load("tiny.onnx")
-tf_rep = prepare(onnx_model)
-tf_rep.export_graph("model_tf")
-
-converter = tf.lite.TFLiteConverter.from_saved_model("model_tf")
-tflite_model = converter.convert()
-
-with open("model.tflite", "wb") as f:
-    f.write(tflite_model)
+import openvino as ov
+ov.convert_model('.onnx')
